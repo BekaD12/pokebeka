5976ff9465a504ca78cacccf72d2eb95a4f4a540
Benoit K <28750720+BekaD12@users.noreply.github.com>
2023-09-21 18:36:38 +0200

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,7 +15,10 @@ async function toggleLocales() {
 <template>
   <nav flex="~ gap-6" m-8 justify-between>
     <router-link class="navbar-logo" to="/" aria-label="Pokedex">
-      <h1><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48"><mask id="ipSPokeballOne0"><g fill="none"><path stroke="#fff" stroke-linejoin="round" stroke-width="4" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z" /><circle cx="24" cy="24" r="6" fill="#fff" stroke="#fff" stroke-linejoin="round" stroke-width="4" /><path stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M30 24h14M4 24h14" /><circle cx="24" cy="24" r="2" fill="#000" /></g></mask><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSPokeballOne0)" /></svg> Pokédex</h1>
+      <h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48"><mask id="ipSPokeballOne0"><g fill="none"><path stroke="#fff" stroke-linejoin="round" stroke-width="4" d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z" /><circle cx="24" cy="24" r="6" fill="#fff" stroke="#fff" stroke-linejoin="round" stroke-width="4" /><path stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M30 24h14M4 24h14" /><circle cx="24" cy="24" r="2" fill="#000" /></g></mask><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSPokeballOne0)" /></svg>
+        PokéBeka - Pokédex
+      </h1>
     </router-link>
     <button icon-btn :title="t('button.toggle_dark')" @click="toggleDark()">
       <div i="carbon-sun dark:carbon-moon" />
